3d1d3141af0249f316e195a28627138394dae6fe
luisvinatea <luisvinatea@icloud.com>
2025-04-03 00:24:58 -0300

    Update with latest changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2629395667"
+    serviceWorkerVersion: "730083907"
   }
 });
